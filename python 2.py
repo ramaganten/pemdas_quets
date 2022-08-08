2b99acee7f5b4c5ea439e8e298d072ecdaed246d
rama12 <110508841+ramaganten@users.noreply.github.com>
2022-08-07 18:52:00 -0700

    Add files via upload

---
+++ python 2.py
@@ -0,0 +1,21 @@
+# belajar input
+nama= "ramadhan "#ramadhan adalah ,nama adalah variebel
+umur = 15 tahun
+alamat: " cimanggis  kota depok ''
+print  (nama )
+print ( umur )
+
+print (alamat)
+
+
+print ( "nama saya adalah " + nama saya " str umur +" alamat saya  " +alamat)
+print("nama saya adalah ''+ nama )
+
+
+
+
+
+
+
+
+
---
+++ python1.py
@@ -0,0 +1,12 @@
+#Belajar Output
+
+print (''hello word" )
+PRINT (''NAMA SAYA RAMADHAN WAHYU SAPUTRA'') 
+Print (''berasal  dari indonesia")
+print (" hobi saya pemain bola")
+print ("saya adalah pelajar ")
+print ('cita - cita saya pemain sepak bola)
+print ("saya adalah pelajar"")
+print ("tinggal di depok ")
+print (" tempat lahir saya depok")
+print("saya lahir tanggal 13 oktober")
